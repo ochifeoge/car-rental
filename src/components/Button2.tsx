7b076ead1f0659c8b3a7cae86dd187e5cb2737ba
Ochife Ogechukwu <ochifeoge@gmail.com>
2025-04-27 14:08:41 +0100

    .

--- src/components/Button2.tsx
+++ src/components/Button2.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Button2 = ({ label }: { label: string }) => {
   return (
     <button className="border border-secondary backdrop-blur-lg text-secondary capitalize px-4 py-2 cursor-pointer">
